5f90863b156bf4ea5c5ff92d4162be8c8ad3a3bf
Nitesh <niteshvashist17@gmail.com>
2025-06-28 22:11:34 +0530

    few final fixes 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Serena Blake - Clinical Psychologist',
+  description: 'Internship Project',
+  generator: 'Nitesh Vashist',
 }
 
 export default function RootLayout({
